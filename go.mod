c3e158e2e4ae1aa6aa62437dd47d6cf4fcec1692
Wellington Gadelha <wellingtongadelha@Informeai.local>
2021-07-22 15:53:57 -0300

    add enviroment of main

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/informeai/gclock
 
 go 1.16
 
-require github.com/pterm/pterm v0.12.29
+require (
+	github.com/faiface/beep v1.0.2
+	github.com/pterm/pterm v0.12.29
+)
--- main.go
+++ main.go
@@ -1,10 +1,36 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"github.com/faiface/beep"
+	"github.com/faiface/beep/mp3"
+	"github.com/faiface/beep/speaker"
 	"github.com/pterm/pterm"
+	"os"
+	"os/exec"
+	"runtime"
 	"time"
 )
+
+func playSound() {
+	f, err := os.Open(os.Getenv("song"))
+	if err != nil {
+		panic(err)
+	}
+	defer f.Close()
+	streamer, format, err := mp3.Decode(f)
+	if err != nil {
+		panic(err)
+	}
+	defer streamer.Close()
+	speaker.Init(format.SampleRate, format.SampleRate.N(time.Second/10))
+	done := make(chan bool)
+	speaker.Play(beep.Seq(streamer, beep.Callback(func() {
+		done <- true
+	})))
+	<-done
+}
 
 func loopClock(seconds int) {
 	//write clock in center terminal
@@ -21,33 +47,100 @@ func loopClock(seconds int) {
 		area.Update(str)
 		time.Sleep(time.Second)
 		if seconds == 0 {
+
 			break
 		}
 		seconds--
 	}
 	area.Stop()
+	//play sound
+	playSound()
 
 }
 func run(pomo string) {
 	//loopClock
 	if pomo == "pomo" {
-		loopClock(1500)
-	} else {
-		loopClock(300)
+		// loopClock(1500)
+		loopClock(10)
+	} else if pomo == "repose" {
+		// loopClock(300)
+		loopClock(5)
 	}
 }
+func next(count int) {
+	pterm.DefaultBasicText.WithStyle(pterm.NewStyle(pterm.FgCyan)).Print("\U0001F449 [1]NEW POMODORO, [2]REPOSE, [3]QUIT: ")
+	//reader
+	reader := bufio.NewReader(os.Stdin)
+	char, _, err := reader.ReadRune()
+	if err != nil {
+		panic(err)
+	}
 
-func main() {
+	switch char {
+	case '1':
+		count++
+		Start(count, "pomo")
+	case '2':
+		Start(count, "repose")
+	case '3':
+		os.Exit(0)
+	default:
+
+	}
+
+}
+
+func initialise(count int, pomo string) {
 	//header
+	fmt.Println("\n\n")
 	pterm.DefaultCenter.Println(pterm.FgYellow.Sprint("\U000023F1 GCLOCK"))
 	pterm.DefaultCenter.Println(pterm.FgLightYellow.Sprint("Clock for recording time using the pomodoro technique."))
 
 	pterm.DefaultCenter.Println(pterm.FgCyan.Sprint("made with \U00002764 by wellington gadelha"))
 	fmt.Println("\n\n")
-	var pomo int = 0
-	pterm.DefaultCenter.Println("Pomodoros: ", pterm.FgLightCyan.Sprint(pomo))
-	//state pomodoro
-	var pomodoroOrRepose string = "pomo"
-	run(pomodoroOrRepose)
+
+	pterm.DefaultCenter.Println("Pomodoros: ", pterm.FgLightCyan.Sprint(count))
+	if count == 0 {
+		area, err := pterm.DefaultArea.WithCenter().Start()
+		if err != nil {
+			panic(err)
+		}
+		str, err := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString(fmt.Sprint("25:00"))).Srender()
+		if err != nil {
+			panic(err)
+		}
+		area.Update(str)
+
+	} else {
+		run(pomo)
+	}
+}
+func clear() {
+	if runtime.GOOS == "windows" {
+		cmd := exec.Command("cls")
+		err := cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+	} else {
+		cmd := exec.Command("clear")
+		cmd.Stdout = os.Stdout
+		err := cmd.Run()
+		if err != nil {
+			panic(err)
+		}
+	}
+}
+
+func Start(count int, pomo string) {
+	clear()
+	initialise(count, pomo)
+	next(count)
+}
+
+func main() {
+	os.Setenv("song", "./song/alarm.mp3")
+	count := 0
+	Start(count, "pomo")
 
 }
