464a3cd8fe918cda5d6e393855e92dc81b7b4081
Wellington Gadelha <wellingtongadelha@Informeai.local>
2021-07-22 10:12:30 -0300

    create function base of clock

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/informeai/gclock
 
 go 1.16
+
+require github.com/pterm/pterm v0.12.29
--- main.go
+++ main.go
@@ -1,5 +1,53 @@
 package main
 
+import (
+	"fmt"
+	"github.com/pterm/pterm"
+	"time"
+)
+
+func loopClock(seconds int) {
+	//write clock in center terminal
+	area, err := pterm.DefaultArea.WithCenter().Start()
+	if err != nil {
+		panic(err)
+	}
+	for {
+
+		str, err := pterm.DefaultBigText.WithLetters(pterm.NewLettersFromString(fmt.Sprintf("%v:%v", seconds/60, seconds%60))).Srender()
+		if err != nil {
+			panic(err)
+		}
+		area.Update(str)
+		time.Sleep(time.Second)
+		if seconds == 0 {
+			break
+		}
+		seconds--
+	}
+	area.Stop()
+
+}
+func run(pomo string) {
+	//loopClock
+	if pomo == "pomo" {
+		loopClock(1500)
+	} else {
+		loopClock(300)
+	}
+}
+
 func main() {
+	//header
+	pterm.DefaultCenter.Println(pterm.FgYellow.Sprint("\U000023F1 GCLOCK"))
+	pterm.DefaultCenter.Println(pterm.FgLightYellow.Sprint("Clock for recording time using the pomodoro technique."))
+
+	pterm.DefaultCenter.Println(pterm.FgCyan.Sprint("made with \U00002764 by wellington gadelha"))
+	fmt.Println("\n\n")
+	var pomo int = 0
+	pterm.DefaultCenter.Println("Pomodoros: ", pterm.FgLightCyan.Sprint(pomo))
+	//state pomodoro
+	var pomodoroOrRepose string = "pomo"
+	run(pomodoroOrRepose)
 
 }
